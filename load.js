623fbcebb0293fc0cef0061a5218611a6dd95df5
hy-PC\hy <529622672@qq.com>
2019-11-25 17:38:03 +0800

    下拉刷新

---
+++ load.js
@@ -0,0 +1,62 @@
+/*
+    var load = new chzLoad();
+    load.init({
+        loadHeight: '100',
+        box: '.box1',
+        'chzEnd': function(){
+            getData();
+        }
+    })
+*/
+
+window.chzDropDown = (function() {
+    var MobileLoad = function() {
+        this.loadHeight,
+        this.chzEnd = this.chzEnd
+    }
+    MobileLoad.prototype = {
+        init: function(params){
+            this.params = params;
+            this.loadHeight = params.loadHeight;
+            this.chzEnd = params.chzEnd;
+            this.chzContent = document.querySelector(params.box);
+            this.chzIcon = document.querySelector('.chzIcon');
+            this.startY = null;   //下拉开始的位置
+            this.bindEvent();
+        },
+        bindEvent: function(){
+            var _self = this;
+            document.body.addEventListener('touchstart', function(e){
+                _self.startY = e.targetTouches[0].clientY;
+            });
+            document.body.addEventListener('touchmove', function(ev){
+                var moveY = parseInt(ev.targetTouches[0].clientY - _self.startY);   //下拉的距离
+                if(moveY >= _self.loadHeight){
+                    moveY = _self.loadHeight;
+                    setTimeout(function(){
+                        _self.chzIcon.style.display='block';
+                        _self.chzIcon.style.top = moveY/2+'px';
+                    },100);
+                }else{
+                    moveY = moveY;
+                };
+                _self.chzContent.style.transform = 'translate3d(0px, '+moveY+'px, 0px)';
+            });
+            document.body.addEventListener('touchend', function(ev){
+                //下拉结束的位置
+                var endY = parseInt(ev.changedTouches[0].clientY - _self.startY);
+              //  if(!endY)return false;
+                if(endY > _self.loadHeight){
+                    setTimeout(function(){
+                        //作为接口的请求
+                        _self.chzEnd();
+                    },500);
+                };
+                _self.chzIcon.style.top = '0px';
+                _self.chzIcon.style.display='none';
+                _self.chzContent.style.transform = 'translate3d(0px, 0px, 0px)';
+            });
+        }
+    };
+    return MobileLoad;
+})();
